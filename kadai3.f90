8b83165991eb54ea9eeecb9f764904e1b9158f0c
Takanobu Amano <amano@eps.s.u-tokyo.ac.jp>
2026-04-18 23:54:27 +0900

    Fix kadai3.f90: proper recursive Hilbert curve with LDR/URD/RUL/DLU

--- kadai3.f90
+++ kadai3.f90
@@ -1,47 +1,117 @@
 program assignment3
   implicit none
+  
   integer :: n
+  
   write(0, fmt='(a)', advance='no') 'Input the order of Hilbert curve n : '
   read(*, *) n
+  
   call generate_hilbert(n)
+  
 contains
+
   subroutine generate_hilbert(n)
     implicit none
     integer, intent(in) :: n
-    integer :: size_grid, total, i, x, y
+    
+    integer :: ix, iy, size_grid
     real(8) :: step
+    
     size_grid = 2**(n + 1)
     step = 1.0_8 / size_grid
-    total = size_grid * size_grid
-    do i = 0, total - 1
-      call d2xy(i, n + 1, x, y)
-      write(*, '(2(f20.10))') (x + 0.5_8) * step, (y + 0.5_8) * step
-    end do
-  end subroutine
-  
-  subroutine d2xy(d, n, x, y)
+    
+    ! Start at position that keeps curve in [0, 1]
+    ! The LDR curve extends left and down from start, so we start at top-right quadrant
+    ix = size_grid - 1
+    iy = size_grid - 1
+    
+    ! Generate Hilbert curve starting with LDR orientation
+    call ldr(n + 1, ix, iy, step)
+    
+  end subroutine generate_hilbert
+
+  recursive subroutine ldr(m, x, y, step)
     implicit none
-    integer, intent(in) :: d, n
-    integer, intent(out) :: x, y
-    integer :: t, rx, ry, s, temp, i
-    x = 0; y = 0
-    t = d
-    do i = 0, n - 1
-      s = 2**i
-      rx = mod(t, 2)
-      ry = mod(t / 2, 2)
-      if (ry == 0) then
-        if (rx == 1) then
-          x = s - 1 - x
-          y = s - 1 - y
-        end if
-        temp = x
-        x = y
-        y = temp
-      end if
-      x = x + rx * s
-      y = y + ry * s
-      t = t / 4
-    end do
-  end subroutine
-end program
+    integer, intent(in) :: m
+    integer, intent(inout) :: x, y
+    real(8), intent(in) :: step
+    
+    if (m == 0) then
+      write(*, '(2(f20.10))') (x + 0.5_8) * step, (y + 0.5_8) * step
+      return
+    end if
+    
+    call dlu(m - 1, x, y, step)
+    x = x - 1                       ! L: move left
+    call ldr(m - 1, x, y, step)
+    y = y - 1                       ! D: move down
+    call ldr(m - 1, x, y, step)
+    x = x + 1                       ! R: move right
+    call urd(m - 1, x, y, step)
+    
+  end subroutine ldr
+
+  recursive subroutine urd(m, x, y, step)
+    implicit none
+    integer, intent(in) :: m
+    integer, intent(inout) :: x, y
+    real(8), intent(in) :: step
+    
+    if (m == 0) then
+      write(*, '(2(f20.10))') (x + 0.5_8) * step, (y + 0.5_8) * step
+      return
+    end if
+    
+    call rul(m - 1, x, y, step)
+    y = y + 1                       ! U: move up
+    call urd(m - 1, x, y, step)
+    x = x + 1                       ! R: move right
+    call urd(m - 1, x, y, step)
+    y = y - 1                       ! D: move down
+    call ldr(m - 1, x, y, step)
+    
+  end subroutine urd
+
+  recursive subroutine rul(m, x, y, step)
+    implicit none
+    integer, intent(in) :: m
+    integer, intent(inout) :: x, y
+    real(8), intent(in) :: step
+    
+    if (m == 0) then
+      write(*, '(2(f20.10))') (x + 0.5_8) * step, (y + 0.5_8) * step
+      return
+    end if
+    
+    call urd(m - 1, x, y, step)
+    x = x + 1                       ! R: move right
+    call rul(m - 1, x, y, step)
+    y = y + 1                       ! U: move up
+    call rul(m - 1, x, y, step)
+    x = x - 1                       ! L: move left
+    call dlu(m - 1, x, y, step)
+    
+  end subroutine rul
+
+  recursive subroutine dlu(m, x, y, step)
+    implicit none
+    integer, intent(in) :: m
+    integer, intent(inout) :: x, y
+    real(8), intent(in) :: step
+    
+    if (m == 0) then
+      write(*, '(2(f20.10))') (x + 0.5_8) * step, (y + 0.5_8) * step
+      return
+    end if
+    
+    call ldr(m - 1, x, y, step)
+    y = y - 1                       ! D: move down
+    call dlu(m - 1, x, y, step)
+    x = x - 1                       ! L: move left
+    call dlu(m - 1, x, y, step)
+    y = y + 1                       ! U: move up
+    call rul(m - 1, x, y, step)
+    
+  end subroutine dlu
+
+end program assignment3
